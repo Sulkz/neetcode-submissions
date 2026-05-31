32f1f100b06148d4df207bd40da706853789b696
Reuel Igbokwe <82598622+Sulkz@users.noreply.github.com>
2026-05-31 12:00:30 +0100

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        new_str = ""
+
+        for char in s:
+            if char.isalnum():
+                new_str += char.lower()
+        
+        l, r = 0 , len(new_str) -1
+
+        while l < r:
+            if new_str[l] != new_str[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+            
+
