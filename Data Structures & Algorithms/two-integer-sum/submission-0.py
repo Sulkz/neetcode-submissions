f949ab3e737bbdaea2976f4229cffe318e2213db
Reuel Igbokwe <82598622+Sulkz@users.noreply.github.com>
2026-08-27 06:05:58 +0100

    Bulk sync: 4 submissions

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++ Data Structures & Algorithms/two-integer-sum/submission-0.py
@@ -1,8 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        check = {}
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-        return []
-        
+            complement = target - nums[i]
+            if complement in check:
+                return [check[complement], i]
+            check[nums[i]] = i
